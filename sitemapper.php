d1ecf058c1ae98b1f8a31623b0ec4706ccfbe1ae
Kyran Gostelow <kyran@gostelow.me>
2017-03-20 22:50:34 +1100

    Main code
    The code needed for the sitemapper

---
+++ sitemapper.php
@@ -0,0 +1,82 @@
+<?php
+
+	$home = $_SERVER['DOCUMENT_ROOT'];
+	$homeurl = $_SERVER['HTTP_HOST'];
+	$urls = array_diff(scandir($home . "/", 1), array('..', '.'));
+	$innerurls = array();
+	$text = "";
+
+	if (isset($_GET['xml']))
+	{
+		$xml = true;
+	}
+	else
+	{
+		$xml = false;
+		$levels = "<span style='margin-left: 40px;'></span>";
+	}
+
+	if ($xml == true)
+	{
+		$text .= "<?xml version='1.0' encoding='UTF-8'?>\n<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>\n";
+	}
+
+	foreach ($urls as $url)
+	{
+		checkurl($url, 0, "/");
+	}
+
+	function checkurl($url, $level, $dir)
+	{
+		global $xml, $text, $levels, $home;
+		if ($xml == true)
+		{
+			$text .= "<url><loc> " . $homeurl . $dir . $url . " </loc></url>\n";
+		}
+		elseif ($xml == false)
+		{
+			$text .= str_repeat($levels, $level) . "<a href='" . $homeurl . $dir . $url . "'> " . $url . "</a><br>";
+		}
+
+		if (another_is_dir($dir . $url))
+		{
+			$innerurls[$url] = array_diff(scandir($home . $dir . $url, 1), array('..', '.'));
+			foreach ($innerurls[$url] as $urld)
+			{
+				checkurl($urld, $level + 1, $dir . $url . "/");
+			}
+		}
+	}
+
+	if ($xml == true)
+	{
+		$text .= "</urlset>";
+		echo "<textarea rows='100' cols='100'>";
+		echo $text;
+		echo "</textarea>";
+		if (isset($_GET['update']))
+		{
+			updatexml();
+		}
+	}
+	else
+	{
+		echo $text;
+	}
+
+	function updatexml()
+	{
+		global $text;
+		$sitemap = fopen("sitemap.xml", "w") or die ("Cannot find sitemap.xml");
+		fwrite($sitemap, $text);
+		fclose($sitemap);
+	}
+
+	// Modified from PHP Manual - is_dir, User comment: sly at noiretblanc dot org
+	function another_is_dir ($file)
+	{ 
+		global $home;
+		return ((fileperms($home . $file) & 0x4000) == 0x4000);
+	}
+
+?>
